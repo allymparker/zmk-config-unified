406399160315a3e22d96e0edf474a53c3cc009f2
Ally Parker <ally@theparkers.xyz>
2024-11-13 09:29:57 +0000

    Revert "2 thumb keys"
    This reverts commit 10d66cdc0ffed81c3b0f2e4b3fdddf7c5147a959.

--- base/combos.h
+++ base/combos.h
@@ -1,40 +1,11 @@
 / {
     combos {
         compatible = "zmk,combos";
-
         combo_updir {
             timeout-ms = <50>;
             key-positions = <RB3 RB4>;
             bindings = <&UPDIR>;
             layers=<0>;
-        };
-
-        combo_tab {
-            timeout-ms = <50>;
-            key-positions = <RM1 RM2>;
-            bindings = <&kp TAB>;
-            layers=<0>;
-        };
-
-        combo_del {
-            timeout-ms = <50>;
-            key-positions = <RB1 RB2>;
-            bindings = <&kp DEL>;
-            layers=<0>;
-        };
-
-        combo_und {
-            timeout-ms = <50>;
-            key-positions = <LM1 LM2>;
-            bindings = <&kp UNDER>;
-            layers=<1>;
-        };
-
-        combo_dot {
-            timeout-ms = <50>;
-            key-positions = <RM1 RM2>;
-            bindings = <&kp DOT>;
-            layers=<3>;
         };
 
     };
