25e9f15598d2c9a0400095678fa4929990602dc3
Ally Parker <ally@theparkers.xyz>
2025-04-27 11:42:46 +0100

    Move combos

--- base/combos.h
+++ base/combos.h
@@ -6,70 +6,84 @@
             timeout-ms = <50>;
             key-positions = <RB3 RB4>;
             bindings = <&UPDIR>;
-            layers=<0>;
+            layers=<BASE>;
         };
 
         combo_zoom_in {
             timeout-ms = <50>;
             key-positions = <RT3 RT4>;
             bindings = <&MC_ZIN>;
-            layers=<0>;
+            layers=<BASE>;
         };
 
         combo_screenshot {
             timeout-ms = <50>;
             key-positions = <RM3 RM4>;
             bindings = <&MC_PSC>;
-            layers=<0>;
+            layers=<BASE>;
         };
 
         combo_zoom_out {
             timeout-ms = <50>;
-            key-positions = <RB3 RB4>;
+            key-positions = <RT2 RT3>;
             bindings = <&MC_ZOT>;
-            layers=<0>;
+            layers=<BASE>;
         };
 
         combo_volup {
             timeout-ms = <50>;
             key-positions = <LT3 LT4>;
             bindings = <&CVUP>;
-            layers=<0>;
+            layers=<BASE>;
         };
 
         combo_pp {
             timeout-ms = <50>;
-            key-positions = <LM3 LM4>;
+            key-positions = <LT2 LT3>;
             bindings = <&CPP>;
-            layers=<0>;
+            layers=<BASE>;
         };
 
         combo_voldown {
             timeout-ms = <50>;
             key-positions = <LB3 LB4>;
             bindings = <&CVDN>;
-            layers=<0>;
+            layers=<BASE>;
+        };
+
+        combo_next {
+            timeout-ms = <50>;
+            key-positions = <LT3 LT4>;
+            bindings = <&CNXT>;
+            layers=<NAV>;
+        };
+        
+        combo_prev {
+            timeout-ms = <50>;
+            key-positions = <LB3 LB4>;
+            bindings = <&CPRV>;
+            layers=<NAV>;
         };
 
         combo_jetbrains {
             timeout-ms = <50>;
             key-positions = <LB1 RB1>;
             bindings = <&sl JETBRAINS>;
-            layers=<0>;
+            layers=<BASE>;
         };
 
         combo_flash_L {
             timeout-ms = <50>;
             key-positions = <LT0 LT4>;
             bindings = <&FLASH>;
-            layers=<0>;
+            layers=<BASE>;
         };
 
         combo_flash_R {
             timeout-ms = <50>;
             key-positions = <RT0 RT4>;
             bindings = <&FLASH>;
-            layers=<0>;
+            layers=<BASE>;
         };
 
     };
