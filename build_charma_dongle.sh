c2923e42856c07dd3b3245a9bd4f93143f2adc38
Ally Parker <ally@theparkers.xyz>
2025-08-09 10:31:35 +0100

    Add englmaxi dongle_display

--- build_charma_dongle.sh
+++ build_charma_dongle.sh
@@ -5,9 +5,11 @@ set -e
 
 echo "Building for Charma keyboard with dongle"
 KEYBOARD_NAME="charma"
-BOARD_NAME="seeeduino_xiao_ble"
+PERIPHERAL_BOARD="seeeduino_xiao_ble"
+PERIPHERAL_ADDITIONAL_SHIELDS="rgbled_adapter"
+
 DONGLE_BOARD="nice_nano"
-ADDITIONAL_SHIELDS="rgbled_adapter"
+DONGLE_ADDITIONAL_SHIELDS="dongle_display"
 
 rm -rf .west
 
@@ -17,27 +19,27 @@ export "CMAKE_PREFIX_PATH=/keymap/zephyr:$CMAKE_PREFIX_PATH"
 # Build dongle (central)
 west build -d /build/charma/dongle -p -b "$DONGLE_BOARD" \
   -s /keymap/zmk/app \
-  -- -DSHIELD="${KEYBOARD_NAME}_dongle" \
+  -- -DSHIELD="${KEYBOARD_NAME}_dongle ${DONGLE_ADDITIONAL_SHIELDS}" \
   -DZMK_CONFIG="/keymap/config_$KEYBOARD_NAME"
 
 # Build left peripheral
-west build -d /build/charma/left_peripheral -p -b "$BOARD_NAME" \
+west build -d /build/charma/left_peripheral -p -b "$PERIPHERAL_BOARD" \
   -s /keymap/zmk/app \
-  -- -DSHIELD="${KEYBOARD_NAME}_left_peripheral ${ADDITIONAL_SHIELDS}" \
+  -- -DSHIELD="${KEYBOARD_NAME}_left_peripheral ${PERIPHERAL_ADDITIONAL_SHIELDS}" \
   -DZMK_CONFIG="/keymap/config_$KEYBOARD_NAME" \
   -DCONFIG_ZMK_SPLIT=y \
   -DCONFIG_ZMK_SPLIT_ROLE_CENTRAL=n
 
 # Build right peripheral
-west build -d /build/charma/right_peripheral -p -b "$BOARD_NAME" \
+west build -d /build/charma/right_peripheral -p -b "$PERIPHERAL_BOARD" \
   -s /keymap/zmk/app \
-  -- -DSHIELD="${KEYBOARD_NAME}_right ${ADDITIONAL_SHIELDS}" \
+  -- -DSHIELD="${KEYBOARD_NAME}_right ${PERIPHERAL_ADDITIONAL_SHIELDS}" \
   -DZMK_CONFIG="/keymap/config_$KEYBOARD_NAME" \
   -DCONFIG_ZMK_SPLIT=y \
   -DCONFIG_ZMK_SPLIT_ROLE_CENTRAL=n
 
 # Build settings reset for all boards
-west build -d /build/settings_reset_xiao -p -b "$BOARD_NAME" \
+west build -d /build/settings_reset_xiao -p -b "$PERIPHERAL_BOARD" \
   -s /keymap/zmk/app \
   -- -DSHIELD="settings_reset" \
   -DZMK_CONFIG="/keymap/config_$KEYBOARD_NAME"
@@ -51,5 +53,5 @@ mkdir -p /keymap/firmware/charma_dongle
 cp /build/charma/dongle/zephyr/zmk.uf2 /keymap/firmware/charma_dongle/${KEYBOARD_NAME}_dongle.uf2
 cp /build/charma/left_peripheral/zephyr/zmk.uf2 /keymap/firmware/charma_dongle/${KEYBOARD_NAME}_left_peripheral.uf2
 cp /build/charma/right_peripheral/zephyr/zmk.uf2 /keymap/firmware/charma_dongle/${KEYBOARD_NAME}_right_peripheral.uf2
-cp /build/settings_reset_xiao/zephyr/zmk.uf2 /keymap/firmware/charma_dongle/${BOARD_NAME}_settings_reset.uf2
+cp /build/settings_reset_xiao/zephyr/zmk.uf2 /keymap/firmware/charma_dongle/${PERIPHERAL_BOARD}_settings_reset.uf2
 cp /build/settings_reset_dongle/zephyr/zmk.uf2 /keymap/firmware/charma_dongle/${DONGLE_BOARD}_settings_reset.uf2
